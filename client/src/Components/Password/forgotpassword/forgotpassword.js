bb0d435a844bb3c7fa2933de0449926604f1b5aa
frank99 <cashcold99@gmail.com>
2025-03-21 18:11:31 +0000

    update

--- client/src/Components/Password/forgotpassword/forgotpassword.js
+++ client/src/Components/Password/forgotpassword/forgotpassword.js
@@ -1,66 +1,54 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import './style.css';
+import axios from 'axios'
+import './style.css'
 import 'react-toastify/dist/ReactToastify.css';
-import { toast, ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify'; 
+
 
 class ForgotPassword extends Component {
-    constructor(props) {
-        super(props);
+    constructor(props){
+        super(props)
         this.state = {
-            restartLinkPassword: '',
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-    }
-
-    handleChange = input => event => {
-        this.setState({ [input]: event.target.value });
-    };
-
-    onSubmit = event => {
-        event.preventDefault();
-        const saveRestartLinkPassword = {
-            email: this.state.restartLinkPassword,
-        };
-
-        // Validate email
-        if (!saveRestartLinkPassword.email || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(saveRestartLinkPassword.email)) {
-            return toast.warning("Enter a valid email address", { position: 'top-center' });
+            restartLinkPassword: ''
         }
+        this.handleChange = this.handleChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+    handleChange = input => (event)=>{
+        this.setState({[input]: event.target.value})
+    }
+    onSubmit = (event)=>{
+        event.preventDefault()
+        const saveRestartLinkPassword = {
+            email: this.state.restartLinkPassword
+        }
+        if(!saveRestartLinkPassword.email){
+            return(toast.warning("Enter Email",{position: 'top-center'}))
+        }    
 
-        axios.post("/users/forgotpassword", saveRestartLinkPassword)
-            .then(res => {
-                toast.success("Password reset link sent to your email");
-            })
-            .catch(err => {
-                const errorMessage = err.response?.data || "Failed to send reset link";
-                toast.error(errorMessage);
-            });
-    };
+        axios.post ("/users/forgotpassword",saveRestartLinkPassword)
+        .then(res => (toast.success("Link have sent to email address")))
+        .catch(err => (toast.error(err.response.data)))
 
-    render() {
-        return (
+    }
+ 
+    render() { 
+        return ( 
             <div className='passwordForgot'>
                 <ToastContainer />
                 <div className='mainPassword'>
-                    <h3 className='animate__animated animate__zoomInRight animate__slower'>
-                        <span>RECOVER</span> YOUR PASSWORD <span>VIA</span> INBOX
-                    </h3>
+                    <h3 className='animate__animated animate__zoomInRight animate__slower'><span>RECOVERY</span> YOUR PASSWORD <span>VIA</span> INBOX</h3>
                     <div className='recoverInput animate__animated animate__zoomInLeft animate__slower'>
-                        <input
-                            placeholder='Email'
-                            onChange={this.handleChange("restartLinkPassword")}
-                            name='restartLinkPassword'
-                        />
+                        <input placeholder='email' onChange={this.handleChange("restartLinkPassword")}  name='restartLinkPassword'/>
                     </div>
-                    <button className='btn btn-success' onClick={this.onSubmit}>
-                        Recover Password
-                    </button>
+                    
+                    <button className='btn btn-success' onClick={this.onSubmit}>Recover Password</button>
                 </div>
+               
             </div>
-        );
+         );
     }
 }
-
+ 
 export default ForgotPassword;
