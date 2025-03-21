735e6e5113a8928b9fbcab143f8b7c474bd3d98c
frank99 <cashcold99@gmail.com>
2025-03-21 18:27:54 +0000

    update

--- client/src/Components/Register/register.js
+++ client/src/Components/Register/register.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import './style.css'
-import axios from 'axios'
+import './style.css';
+import axios from 'axios';
 import 'react-toastify/dist/ReactToastify.css';
-import { toast } from 'react-toastify';
-import { ToastContainer } from 'react-toastify';
-
+import { toast, ToastContainer } from 'react-toastify';
 
 class RegisterMain extends Component {
     constructor(props) {
@@ -16,41 +14,34 @@ class RegisterMain extends Component {
             password: '',
             confirm_password: '',
             ip_address: '',
-            checkBox: '',
+            checkBox: false,
             date: '',
             referral: '',
-            restartLinkPassword: ''
-         }
+        };
 
-        this.handleChange = this.handleChange.bind(this)
-        this.onSubmit = this.onSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
-    handleChange = input => (event)=>{
-        this.setState({[input]: event.target.value})
-        
+    handleChange = input => event => {
+        const value = input === 'checkBox' ? event.target.checked : event.target.value;
+        this.setState({ [input]: value });
+    };
+
+    componentDidMount() {
+        const referral = localStorage.getItem('reffer');
+        this.setState({ referral });
+
+        const DateTime = new Date().toString();
+        this.setState({ date: DateTime });
+
+        fetch('https://api.ipify.org/?format=json')
+            .then(res => res.json())
+            .then(data => this.setState({ ip_address: data.ip }));
     }
 
-    componentDidMount(){
-
-        const  referral = localStorage.getItem('reffer')
-        this.setState({
-            referral
-        })
-
-        const DateTime = new Date().toString()
-        this.setState({
-            date: DateTime
-        })
-        
-        fetch('http://api.ipify.org/?format=json').then(res => res.json()).then(data => this.setState({
-            ip_address: data.ip
-        }))
-
-    }
-
-    onSubmit = (event)=>{ 
-        event.preventDefault()
+    onSubmit = event => { 
+        event.preventDefault();
         const SaveNewUser = {
             full_name: this.state.full_name,
             password: this.state.password,
@@ -60,79 +51,91 @@ class RegisterMain extends Component {
             ip_address: this.state.ip_address,
             checkBox: this.state.checkBox,
             confirm_password: this.state.confirm_password,
-            date: this.state.date
-            
-        }
+            date: this.state.date,
+        };
 
-       if(SaveNewUser.full_name.length < 6){
-        toast.warn('Full Name  must be at lest 6')
-        return false
-        }
-        if(SaveNewUser.password.length < 6){
-            toast.warn('password is must be at lest 6')
-            return false
-        }
-       
-        if(!SaveNewUser.full_name || !SaveNewUser.phone || !SaveNewUser.password || !SaveNewUser.confirm_password  || !SaveNewUser.email){
-            toast.error('Please Fill All Field')
+        if (SaveNewUser.full_name.length < 6) {
+            toast.warn("Full Name must be at least 6 characters");
             return false;
         }
-        if(SaveNewUser.password != SaveNewUser.confirm_password){
-            toast.warn('password do not match')
-            return false
-        }
-        if(!SaveNewUser.checkBox){ 
-            toast.warn('Please agree with Terms and conditions')
-            return false
-        }
-       axios.post("/users/register",SaveNewUser).then(res => {toast.success("Register Successful")}).then(res => setTimeout(()=>{
-            window.location="/login"
-        }),5000).catch(err => {toast.error(err.response.data)})
 
-    }
+        if (SaveNewUser.password.length < 6) {
+            toast.warn("Password must be at least 6 characters");
+            return false;
+        }
 
+        if (!SaveNewUser.full_name || !SaveNewUser.phone || !SaveNewUser.password || !SaveNewUser.confirm_password || !SaveNewUser.email) {
+            toast.error("Please fill all fields");
+            return false;
+        }
+
+        if (SaveNewUser.password !== SaveNewUser.confirm_password) {
+            toast.warn("Passwords do not match");
+            return false;
+        }
+
+        if (!SaveNewUser.checkBox) {
+            toast.warn("Please agree with Terms and Conditions");
+            return false;
+        }
+
+        axios.post("/users/register", SaveNewUser)
+            .then(res => {
+                toast.success("Register Successful");
+                setTimeout(() => {
+                    window.location = "/login";
+                }, 5000);
+            })
+            .catch(err => {
+                const errorMessage = err.response?.data || "Registration failed";
+                toast.error(errorMessage);
+            });
+    };
 
     render() { 
         return ( 
             <div className='login__main'>
                 <section className='login__section_1 login__section_1_register'>
-                <ToastContainer/>
+                    <ToastContainer />
                     <div className="login__box_1">
                         <h1><span>Sign</span> up</h1>
                         <h3>Create New Bitcoin4u Account</h3>
-                        <div className="login__forms">
-                            <div className='myForms'>
-                                <input type='text' name='fullName' placeholder='Full Name' onChange={this.handleChange('full_name')}/>
+                        <form onSubmit={this.onSubmit}>
+                            <div className="login__forms">
+                                <div className='myForms'>
+                                    <input type='text' name='fullName' placeholder='Full Name' onChange={this.handleChange('full_name')} />
+                                </div>
+                                <div className='myForms'>
+                                    <input type='email' name='email' placeholder='Email' onChange={this.handleChange('email')} />
+                                </div>
+                                <div className='myForms'>
+                                    <input type='tel' name='phone' placeholder='Phone Number' onChange={this.handleChange('phone')} />
+                                </div>
+                                <div className='myForms'>
+                                    <input type='password' name='password' placeholder='Password' onChange={this.handleChange('password')} />
+                                </div>
+                                <div className='myForms'>
+                                    <input type='password' name='confirm_password' placeholder='Confirm Password' onChange={this.handleChange('confirm_password')} />
+                                </div>
                             </div>
-                            <div className='myForms'>
-                                <input type='email' name='email' placeholder='Email' onChange={this.handleChange('email')}/>
+                            <div className="log__btn">
+                                <div className='upfont'>
+                                    <p>Referral:<br /> {this.state.referral}</p>
+                                    <p>
+                                        <input type='checkbox' name='checkbox' onChange={this.handleChange('checkBox')} /> I agree with Terms and Conditions
+                                    </p>
+                                    <button type="submit" className='btn btn-warning'>CREATE ACCOUNT</button>
+                                </div>
                             </div>
-                            <div className='myForms'>
-                                <input type='tel' name='phone' placeholder='Phone Number' onChange={this.handleChange('phone')}/>
-                            </div>
-                            <div className='myForms'>
-                                 <input type='password' name='password' placeholder='Password' onChange={this.handleChange('password')} />
-                            </div>
-                            <div className='myForms'>
-                                 <input type='password' name='password' placeholder='Confirm Password'  onChange={this.handleChange('confirm_password')}/>
-                            </div>
-                          
-                        </div>
-                        <div className="log__btn">
-                             <div className='upfont'>
-                                 <p> Referral:<br/> {this.state.referral} </p>
-                                <p><input type='radio' name='checkbox'  onChange={this.handleChange('checkBox')}/> I agree with Terms and conditions</p>
-                                <a href='' className='btn btn-warning' onClick={this.onSubmit}>CREATE ACCOUNT</a>
-                             </div>
-                        </div>
+                        </form>
                     </div>
                 </section>
                 <section className='login_section_2'>
-                    <img src={require('../../images/cryptocurrency-3305671_1920.jpg')}/>
+                    <img src={require('../../images/cryptocurrency-3305671_1920.jpg')} alt="Background" />
                 </section>
             </div>
-         );
+        );
     }
 }
- 
+
 export default RegisterMain;
