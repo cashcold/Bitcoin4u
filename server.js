9b912fe89edfa76a3c9ba6c58d1a84a5f1211dc2
frank99 <cashcold99@gmail.com>
2025-03-21 16:24:26 +0000

    update on server

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const userRouter = require('./Router/userRouter')
 const path = require('path')
+const fs = require('fs');
 const cron = require("node-cron")
 const shell = require("shelljs")
 
@@ -51,12 +52,30 @@ io.on('connection', socket => {
 
 app.use('/users', userRouter)
 
+app.use(express.static(path.join(__dirname, "client")));
+
+app.get('/', (req, res) => {
+    const filePath = path.resolve(__dirname, './client/build', 'index.html');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            return console.log(err);
+        }
+
+        data = data.replace(/\$OG_TITLE/g, 'Buy & Sell Bitcoin Instantly')
+                   .replace(/\$OG_DESCRIPTION/g, "Bitcoinu4 makes Bitcoin transactions easy and secure. Buy, sell, and trade crypto seamlessly with the best rates.")
+                   .replace(/\$OG_IMAGE/g, 'https://images.unsplash.com/photo-1609726494499-27d3e942456c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Yml0Y29pbnxlbnwwfHwwfHx8MA%3D%3D'); 
+        res.send(data);
+    });
+});
+
+app.use(express.static("client/build"));
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static("client/build"))
+    app.use(express.static("client/build"));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
+
 
 server.listen(PORT, () => {
     console.log(`server is running on local Port Number ${PORT}`)
